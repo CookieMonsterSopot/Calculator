5c87ae31c8458cabb3a2ffca59b41d0da07c7707
CookieMonsterSopot <114802117+CookieMonsterSopot@users.noreply.github.com>
2022-12-31 16:36:36 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
-// This function clear all the values
+// Clear all the values
 function clearScreen() {
   document.getElementById("result").value = "";
 }
-// This function display values
+// Display values
 function display(value) {
   document.getElementById("result").value += value;
 }
-// This function evaluates the expression and returns result
+// Evaluates the expression and returns result
 function calculate() {
   let p = document.getElementById("result").value;
   let q = eval(p);
   document.getElementById("result").value = q;
-  console.log(p + " = " + q);
+  
+// Create new place for history item
   document.getElementById("History").innerHTML += p + "=" + q + "<br/>";
 }
